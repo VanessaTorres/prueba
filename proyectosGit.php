19dead960b248feb40bda6152738eefa77c03a31
vanedesdecasa <vanessatorres1805@gmail.com>
2016-11-14 17:37:26 -0500

    Comprobar la conexion entre el proyecto local y el proyecto remoto

--- proyectosGit.php
+++ proyectosGit.php
@@ -1,4 +1,5 @@
 <?php
 echo "Prueba para empezar";
 echo "Otra modificacion para crear otro commit";
+echo "Se crea otra modificacion para comprobar la conexion del proyecto local al proyecto remoto",
 ?>
